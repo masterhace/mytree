e5185fba55106afc3489baf069abc5954fc0292a
masterhace <sevan50393@gmail.com>
2025-11-15 21:13:16 +0100

    Improve and add some Javadoc and comments

--- src/main/java/mytree/Element.java
+++ src/main/java/mytree/Element.java
@@ -9,10 +9,10 @@ import org.apache.commons.text.StringEscapeUtils;
 /**
  * An implementation of tree structure.
  * <p>
- * The key of this implementation is the Element class which only has 2 fields: name and content.
- * The type of name is always String, and it permits all types to store in content.
+ * The key of this implementation is the {@code Element} class which only has 2 fields: {@code name} and {@code content}.
+ * The type of {@code name} is always {@code String}, and it permits all types to store in {@code content}.
  * <p>
- * ElementList is another class that extends ArrayList, only designed for storing a list of Element. It's important to know to store an ElementList in content to make a tree structure like XML and JSON.
+ * {@code ElementList} is another class that extends {@code ArrayList}, only designed for storing a list of {@code Element}. It's important to know to store an {@code ElementList} in {@code content} to make a tree structure like XML and JSON.
  *
  * @author  Me
  * @see     ElementList
@@ -21,22 +21,22 @@ import org.apache.commons.text.StringEscapeUtils;
 public class Element implements Cloneable {
 
     /**
-     * The name of the Element
+     * The name of the {@code Element}.
      */
 
     public String name;
 
     /**
-     * The content of the Element.
+     * The content of the {@code Element}.
      * <p>
-     * It can store everything, but only ElementList and simple types like String and Integer is recommended.
-     * It can also store ElementList which stores a list of Element
+     * It can store everything, but only {@code ElementList} and simple types like {@code String} and {@code Integer} is recommended.
+     * It can also store {@code ElementList} which stores a list of {@code Element}.
      */
 
     public Object content;
 
     /**
-     * Constructs an Element with name
+     * Constructs an {@code Element} with {@code name}.
      */
 
     public Element(String name) {
@@ -44,7 +44,7 @@ public class Element implements Cloneable {
     }
 
     /**
-     * Constructs an Element with name and content
+     * Constructs an {@code Element} with {@code name} and {@code content}.
      */
 
     public Element(String name, Object content) {
@@ -53,11 +53,12 @@ public class Element implements Cloneable {
     }
 
     /**
-     * The method clone() deep copies the Element.
+     * The {@code clone} method deep copies the {@code Element}.
      * <p>
-     * The objects other than ElementList stored in content would get shallow copied since they could be unchangeable.
+     * The objects other than {@code ElementList} stored in {@code content} would get shallow copied since they could be unchangeable.
      * <p>
-     * The ElementList stored in content would get deep copied recursively.
+     * The {@code ElementList} stored in {@code content} would get deep copied recursively.
+     * @return a clone of this {@code Element}.
      */
 
     @Override
@@ -77,7 +78,8 @@ public class Element implements Cloneable {
     }
 
     /**
-     * Returns the hash code of the Element
+     * Returns the hash code of this {@code Element}.
+     * @return a hash code value for this {@code Element}.
      */
 
     @Override
@@ -86,7 +88,8 @@ public class Element implements Cloneable {
     }
 
     /**
-     * Compare two Elements
+     * Compares two {@code Element}.
+     * @return {@code true} if the given {@code Object} is equal to this {@code Element}.
      */
 
     @Override
@@ -106,6 +109,11 @@ public class Element implements Cloneable {
       Help methods to check content
      */
 
+    /**
+     * Returns the {@code Class} object of {@code content}.
+     * @return The {@code Class} object that represents the runtime class of {@code content}.
+     */
+
     public Class<?> getContentClass() {
         if (isContentNull()) {
             return null;
@@ -114,51 +122,105 @@ public class Element implements Cloneable {
         }
     }
 
+    /**
+     * Check if {@code content} is null.
+     * @return {@code true} if {@code content} is null.
+     */
+
     public boolean isContentNull() {
         return content == null;
     }
+
+    /**
+     * Check if {@code content} is the given type.
+     * @param c the {@code Class} object.
+     * @return {@code true} if {@code content} is the given type.
+     */
 
     public boolean isContentClass(Class<?> c) {
         return getContentClass() == c;
     }
 
     /*
-      Help methods to access content to avoid casting
-      Exception: all methods would throw ClassCastException
+      Access content when its type is ElementList
      */
 
-    /*
-      Access content when its type is ElementList
+    /**
+     * Adds the given {@code Element} to the {@code ElementList} stored in {@code content}.
+     * @param e the {@code Element} object to be added.
      */
 
     public void addChild(Element e) {
         getChildren().add(e);
     }
 
+    /**
+     * Adds the given {@code Element} to the {@code ElementList} stored in {@code content} with index.
+     * @param e the {@code Element} object to be added.
+     * @param index index at which the given {@code Element} is to be inserted.
+     */
+
     public void addChild(Element e, int index) {
         getChildren().add(index, e);
     }
 
+    /**
+     * Removes the {@code Element} from the {@code ElementList} stored in {@code content} with index.
+     * @param index the index of the {@code Element} object to be added.
+     * @return the {@code Element} that was removed from the {@code ElementList}.
+     */
+
     public Element removeChild(int index) {
         return getChildren().remove(index);
-        // Remove while return
     }
+
+    /**
+     * Removes first occurrence of the given {@code Element} from the {@code ElementList} stored in {@code content}.
+     * @param e the {@code Element} object to be removed.
+     * @return {@code true} if the {@code ElementList} contained the given {@code Element}.
+     */
 
     public boolean removeChild(Element e) {
         return getChildren().remove(e);
     }
 
+    /**
+     * Returns the given index of {@code Element} from {@code ElementList} stored in {@code content}.
+     * @param index the index of the {@code Element} object to be gotten.
+     * @return the {@code Element} at the given index of the {@code ElementList}.
+     */
+
     public Element getChild(int index) {
         return getChildren().get(index);
     }
+
+    /**
+     * Replaces the given index of {@code Element} from {@code ElementList} stored in {@code content} with the given {@code Element} object.
+     * @param index the index of the {@code Element} object to be replaced.
+     * @param e the given {@code Element} to be stored.
+     * @return the replaced {@code Element}.
+     */
 
     public Element setChild(int index, Element e) {
         return getChildren().set(index, e);
     }
 
+    /**
+     * Modifies the given index of {@code Element} from {@code ElementList} stored in {@code content} with the given {@code name}.
+     * @param index the index of the {@code Element} object to be replaced.
+     * @param name the given {@code name} to be stored.
+     */
+
     public void modChild(int index, String name) {
         getChild(index).name = name;
     }
+
+    /**
+     * Modifies the given index of {@code Element} from {@code ElementList} stored in {@code content} with the given {@code name} and {@code Object}.
+     * @param index the index of the {@code Element} object to be replaced.
+     * @param name the given {@code name} to be stored.
+     * @param o the given {@code Object} to be stored.
+     */
 
     public void modChild(int index, String name, Object o) {
         getChild(index).name = name;
